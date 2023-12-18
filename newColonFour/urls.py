9e7bc5d142b964b4053c61952996e9420502b56b
bryan <bb222qj@student.lnu.se>
2023-12-18 12:46:02 +0100

    first commit

---
+++ newColonFour/urls.py
@@ -0,0 +1,8 @@
+# yourapp/urls.py
+from django.urls import path
+from .views import HomePageView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    # Add other URL patterns as needed
+]
---
+++ newColonFour/views.py
@@ -0,0 +1,5 @@
+# yourapp/views.py
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'base.html'
